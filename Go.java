edbcfc32c772e0ea9ca671ca5c90e15a445e3c02
BrianLim167 <blim167@gmail.com>
2016-12-23 15:33:11 -0500

    began writing setup window

---
+++ Go.java
@@ -0,0 +1,29 @@
+import javax.swing.*;
+import java.awt.*;
+public class Go extends JFrame {
+    private Container pane;
+    private JLabel dimensionsL,handicapL,komiL,title;
+    private JComboBox dimensions,handicap;
+    private JTextField komi;
+ 
+    //CONSTRUCTOR SETS EVERYTHING UP
+    public Go() {
+	this.setTitle("Go Setup");
+	this.setSize(600,400);
+	this.setLocation(500,400);
+	this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+    
+	pane = this.getContentPane();
+	pane.setLayout(new FlowLayout());
+     
+	String[]sizes = {"19x19","13x13","9x9","25x25","21x21","5x5"};
+
+	komi = new JTextField("6.5");
+    }
+
+    public static void main(String[] args) {
+	Go g = new Go();
+	g.setVisible(true);
+    }
+}
+
