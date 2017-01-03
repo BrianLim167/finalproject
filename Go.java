64866a7c521d6d3bbe9b3711bcb2a1cf19a97752
Penn Wu <penn.z.wu@gmail.com>
2017-01-03 06:36:18 -0500

    Added in button and gave it a command

--- Go.java
+++ Go.java
@@ -1,10 +1,13 @@
 import javax.swing.*;
 import java.awt.*;
-public class Go extends JFrame {
+import java.awt.event.*;
+
+public class Go extends JFrame implements ActionListener {
     private Container pane;
     private JLabel dimensionsL,handicapL,komiL,title;
     private JComboBox dimensions,handicap;
     private JTextField komi;
+    private JButton play;
  
     //CONSTRUCTOR SETS EVERYTHING UP
     public Go() {
@@ -14,12 +17,27 @@ public class Go extends JFrame {
 	this.setDefaultCloseOperation(EXIT_ON_CLOSE);
     
 	pane = this.getContentPane();
-	pane.setLayout(new FlowLayout());
+	pane.setLayout(new BoxLayout(pane, BoxLayout.Y_AXIS));
      
 	String[]sizes = {"19x19","13x13","9x9","25x25","21x21","5x5"};
 
+	komiL = new JLabel("Komi");
 	komi = new JTextField("6.5");
+	JButton play = new JButton("PLAY");
+	play.addActionListener(this);
+	play.setActionCommand("Boat");
+	
+	pane.add(komiL);
 	pane.add(komi);
+	pane.add(play);
+    }
+
+    public void actionPerformed(ActionEvent e) {
+	String event = e.getActionCommand();
+	if (event.equals("Boat")) {
+	    String s = "tree fiddy";
+	    komiL.setText(s);
+	}
     }
 
     public static void main(String[] args) {
